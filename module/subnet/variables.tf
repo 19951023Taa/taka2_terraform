fa9270248fcbe22906927661c5b8d514e76760b3
19951023Taa <takamaaa777@gmail.com>
2023-01-15 18:36:32 +0900

    test

---
+++ module/subnet/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_id" {
+  default = "vpc-0d65cd984ba4276b1"
+}
+
+variable "cidr_block" {
+  
+}
+
+variable "availability_zone" {
+  
+}
+
+variable "project" {
+  default = "terra"
+}
+
+variable "env" {
+  
+}
+
+variable "subnetname" {
+  
+}
--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,23 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+module "pub_subnet_A" {
+ source = "./module/subnet"
+  
+  vpc_id = var.vpc_id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+  project = var.project
+  env = var.env
+  subnetname = "public-A"
+}
+
+module "pub_subnet_C" {
+ source = "./module/subnet"
+  
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1c"
+  project = var.project
+  env = var.env
+  subnetname = "public-C"
+}
