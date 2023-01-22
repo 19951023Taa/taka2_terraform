0a36c3592b310b93bfe4de016d7325ce65830fb3
19951023Taa <takamaaa777@gmail.com>
2023-01-22 16:12:35 +0900

    sg_in_ruleを変更

--- module/security_group/main.tf
+++ module/security_group/main.tf
@@ -13,8 +13,9 @@ resource "aws_security_group_rule" "in_this" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["122.222.134.87/32"]
+  cidr_blocks       = ["59.138.66.7/32"]
   security_group_id = aws_security_group.this.id
+  
 }
 
 
